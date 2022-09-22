8246dcd6aaaf60b58d5ab49dd752814d3a3c65ad
Francis Li <mli@dminc.com>
2022-09-22 14:41:51 -0400

    fixed env

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
 export GITHUB_USERNAME="Freedom4501"
-export GITHUB_REPO="bootcamp-2022-sls-api-Freedom4501"
+export GITHUB_REPO="bootcamp-2022-sls-api-li"
 export BUCKET_NAME="lim5-bootcamp-2022-bucket" #username must be all lowercase
